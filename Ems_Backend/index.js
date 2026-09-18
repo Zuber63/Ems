b0580556fa511519ab772575841c091e808c0249
Zuber Saifi <royalsaifi63@gmail.com>
2026-09-18 21:37:55 +0530

    Connect frontend with backend

--- Ems_Backend/index.js
+++ Ems_Backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:"https://ems-frontend-seven-tau.vercel.app",
         methods:["GET","POST","PUT","DELETE"],
     }
 ));
