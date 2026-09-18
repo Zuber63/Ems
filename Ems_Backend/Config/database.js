66277091a8ae75f4f886e8e3bd5e4f18c402a0b2
Zuber Saifi <royalsaifi63@gmail.com>
2026-09-18 20:56:11 +0530

    Fix backend start command

--- Ems_Backend/Config/database.js
+++ Ems_Backend/Config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
  exports.connectDB=async()=>{
-    mongoose.connect("mongodb://localhost:27017/Ems").then(()=>{
+    mongoose.connect("mongodb+srv://Zuber786:Zuber786@cluster0.zyjpvzf.mongodb.net/Ems?appName=Cluster0").then(()=>{
 console.log("connect to mongo db")
     }).catch(()=>{
 console.log("error found while connecting to mongo db")
